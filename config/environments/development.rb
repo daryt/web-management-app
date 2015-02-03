cb89dc60693c96a9620a533c6209c0e1dd56c79e
Pavan <pt4189@gmail.com>
2015-02-04 05:11:35 +0530

    crypsis.tester@gmail.com is set as the email for forgot password.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,15 @@ WebManagementApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com" ,
+    port: 587,
+    domain: "gmail.com",
+    user_name: "crypsis.tester@gmail.com",
+    password: "crypsis123456",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
